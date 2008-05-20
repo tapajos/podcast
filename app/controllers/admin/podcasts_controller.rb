8228073f0c7a51ad3f132b398e80cf37366356db
Marcos Tapajos <tapajos@gmail.com>
2008-05-20 19:48:09 -0300

    Adding Authentication

--- app/controllers/admin/podcasts_controller.rb
+++ app/controllers/admin/podcasts_controller.rb
@@ -1,2 +1,6 @@
 class Admin::PodcastsController < ResourceController::Base
+  
+  include AuthenticatedSystem
+  before_filter :login_required
+  
 end
--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 1) do
+ActiveRecord::Schema.define(:version => 2) do
 
   create_table "podcasts", :force => true do |t|
     t.integer  "number"
@@ -24,5 +24,19 @@ ActiveRecord::Schema.define(:version => 1) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  create_table "users", :force => true do |t|
+    t.string   "login",                     :limit => 40
+    t.string   "name",                      :limit => 100, :default => ""
+    t.string   "email",                     :limit => 100
+    t.string   "crypted_password",          :limit => 40
+    t.string   "salt",                      :limit => 40
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "remember_token",            :limit => 40
+    t.datetime "remember_token_expires_at"
+  end
+
+  add_index "users", ["login"], :name => "index_users_on_login", :unique => true
 
 end
