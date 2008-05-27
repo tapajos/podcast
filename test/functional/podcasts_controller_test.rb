552a0a9fcfd2f93d258088128574fd19e8eb2aa6
Marcos Tapajos <tapajos@gmail.com>
2008-05-27 18:25:39 -0300

    Adding test to permalink.

--- test/functional/podcasts_controller_test.rb
+++ test/functional/podcasts_controller_test.rb
@@ -15,4 +15,14 @@ class PodcastsControllerTest < ActionController::TestCase
     assert_equal 1, assigns(:podcast).id
   end
   
+  def test_method_missing_when_permalink_is_not_found
+    assert_raise(ActionController::UnknownAction) { get :not_found }
+  end
+  
+  def test_method_missing_when_permalink_is_found
+    get "podcast-17-rpb"
+    assert_equal podcasts(:podcasts_001), assigns(:podcast)
+    assert_template "show"  
+  end
+  
 end
